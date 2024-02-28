3d9c738adfbda6b349ae95ea1b844cb3a5494786
Javi Rute <108191217+JaviRute@users.noreply.github.com>
2024-02-28 17:21:33 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,28 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-//We import all the necessary files for the different sections and css
 import './index.css';
-import Info from './Info';
-import Career from './Career';
-import Education from './Education';
-import Tech from './Tech';
-import Projects from './Projects';
-import Work from './Work';
-//this is the json file containing all the information for the cv
-import json from './cv-data.json';
+import App from './App.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Here we enter each component that will render each of the sections, and we enter 'json' as props */}
-    <Info data={json}/>
-    <Career data={json}/>
-    <Education data={json}/>
-    <Tech data={json}/>
-    <Projects data={json}/>
-    <Work data={json}/>
-    
+    <App />
   </React.StrictMode>
 );
 
