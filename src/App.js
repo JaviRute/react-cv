dc2f09a04c8de18e8ad50dfabb8d10e3ceba54be
Javi Rute <108191217+JaviRute@users.noreply.github.com>
2024-02-28 17:21:09 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
+//We import all the necessary files for the different sections and css
 import './App.css';
+import './index.css';
+import Info from './Info';
+import Career from './Career';
+import Education from './Education';
+import Tech from './Tech';
+import Projects from './Projects';
+import Work from './Work';
+
+//this is the json file containing all the information for the cv
+import json from './cv-data.json';
 
 function App() {
   return (
     <div className="App">
-      <p>Q pasa man</p>
+      {/* Here we enter each component that will render each of the sections, and we enter 'json' as props */}
+      <Info data={json} />
+      <Career data={json} />
+      <Education data={json} />
+      <Tech data={json} />
+      <Projects data={json} />
+      <Work data={json} />
     </div>
   );
 }
